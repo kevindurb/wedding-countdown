d3bbfe910b1976442803b0e3cb586f9565b4e2d8
Kevin Durbin <kevindurb@gmail.com>
2017-02-04 12:05:39 -0700

    Add progress meter

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
-function debug(x) {
-  var d = document.getElementById('debug');
-  d.textContent = x;
-}
 function render() {
   var dayEl = document.getElementById('days');
   var hourEl = document.getElementById('hours');
   var minuteEl = document.getElementById('minutes');
   var secondEl = document.getElementById('seconds');
-  var progress = document.getElementById('progress');
+  var progressEl = document.getElementById('progress');
 
-  var anniversary = moment('2016-11-23 18:30:00');
+  var engaged = moment('2016-11-23 18:30:00');
   var weddingNight = moment('2017-06-24 22:00:00');
   var today = moment();
   var days = weddingNight.diff(
@@ -30,17 +26,17 @@ function render() {
   var m = parseInt(minutes);
   var s = parseInt(seconds);
 
-  // var total = weddingNight.diff(anniversary);
-  // var progress = today.diff(anniversary);
+  var total = weddingNight.diff(engaged);
+  var progress = today.diff(engaged);
 
   dayEl.textContent = d + ' days';
   hourEl.textContent = h + ' hours';
   minuteEl.textContent = m + ' minutes';
   secondEl.textContent = s + ' seconds';
 
-//   debug(progress / total * 100);
+  var percent = progress / total * 100;
 
-//   progress.value = (progress / total) * 100;
+  progressEl.value = percent;
 }
 
 render();
